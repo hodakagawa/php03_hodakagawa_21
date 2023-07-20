915ff587407d191c5ba2c17440347025f8607002
“hodakagawa” <kikugawa.0320@gmail.com>
2023-07-20 15:56:22 +0900

    submit

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+// １．PHP
+$id = $_GET["ユニーク値"] ?? "";
+if ($id === "") {
+    exit("Invalid ID");
+}
+
+try {
+    $pdo = new PDO('mysql:dbname=gs_submit;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+    exit('DB Connection Error:'.$e->getMessage());
+}
+
+$stmt = $pdo->prepare("DELETE FROM gs_bm_table WHERE ユニーク値=:id");
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$status = $stmt->execute();
+
+if ($status == false) {
+    exit("Error: Delete failed");
+} else {
+    header("Location: select.php");
+    exit();
+}
+?>
---
+++ delete_all.php
@@ -0,0 +1,17 @@
+<?php
+try {
+    $pdo = new PDO('mysql:dbname=gs_submit;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+    exit('DB Connection Error:'.$e->getMessage());
+}
+
+$stmt = $pdo->prepare("DELETE FROM gs_bm_table");
+$status = $stmt->execute();
+
+if ($status == false) {
+    exit("Error: Delete all failed");
+} else {
+    header("Location: select.php");
+    exit();
+}
+?>
